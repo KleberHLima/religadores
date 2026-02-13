5cbbe7b86236b03746522c6c1b5c6f2d081b6d2f
KleberHLima <kleber7845@outlook.com>
2026-02-13 10:35:54 -0300

    teste

--- js/script.js
+++ js/script.js
@@ -2,21 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const seletores = document.querySelectorAll(".seletor");
 
-    seletores.forEach(function(seletor) {
+    seletores.forEach(function (seletor) {
 
         const titulo = seletor.querySelector("h2");
-        const container = seletor.querySelector(".cards_img");
-
-        // começa fechado
-        container.style.display = "none";
-
-        titulo.style.cursor = "pointer";
 
         titulo.addEventListener("click", function () {
 
-            const aberto = container.style.display === "grid";
-
-            container.style.display = aberto ? "none" : "grid";
+            seletor.classList.toggle("ativo");
 
         });
 
